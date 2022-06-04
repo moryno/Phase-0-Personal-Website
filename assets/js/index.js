11fba8490d50bdecf1b09f1d250dd938d73a3775
Maurice <mauricenganga41@gmail.com>
2022-06-04 08:41:30 +0300

    Add a footer section with social icons and a copyright time function

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+
+// FUNCTION TO CALCULATE YEAR
+const footer = () => {
+    const year = new Date().getFullYear();
+    const footerText = document.getElementById("footerText");
+    footerText.textContent = `Copyright ⓒ ${year}`;
+}
+footer ();
